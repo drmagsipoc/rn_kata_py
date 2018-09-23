ccbcee028f37146886ca1167b71779f112622c19
p_2 <30770254+drmagsipoc@users.noreply.github.com>
2018-09-24 07:50:23 +0800

    Finished implementation

--- rn.py
+++ rn.py
@@ -1,5 +1,5 @@
 
-def _handle_ones(num):
+def _ones(num):
     try:
         ones = {
             1: 'I',
@@ -12,13 +12,60 @@ def _handle_ones(num):
             8: 'VIII',
             9: 'IX',
         }
+        return ones[num % 10]
     except KeyError:
-        print('invalid handle ones numer')
-    return ones[num]
+        return ''
+
+
+
+def _tens(num):
+    try:
+        tens = {
+            1: 'X',
+            2: 'XX',
+            3: 'XXX',
+            4: 'XL',
+            5: 'L',
+            6: 'LX',
+            7: 'LXX',
+            8: 'LXXX',
+            9: 'XC',
+        }
+        return tens[(num // 10) % 10]
+    except KeyError:
+        return ''
+
+
+def _dreds(num):
+    try:
+        hundreds = {
+            1: 'C',
+            2: 'CC',
+            3: 'CCC',
+            4: 'CD',
+            5: 'D',
+            6: 'DC',
+            7: 'DCC',
+            8: 'DCCC',
+            9: 'CM',
+        }
+        return hundreds[(num // 100) % 10]
+    except KeyError:
+        return ''
+
+
+def _sands(num):
+    try:
+        thousands = {
+            1: 'M',
+            2: 'MM',
+            3: 'MMM',
+        }
+        return thousands[(num // 1000) % 10]
+    except KeyError:
+        return ''
 
 
 def convert(input):
-    ones = _handle_ones(input % 10)
-    return ones
-
-convert(1)
+    roman_numeral = _sands(input) + _dreds(input) + _tens(input)  + _ones(input)
+    return roman_numeral
--- test.py
+++ test.py
@@ -3,9 +3,27 @@ import unittest
 
 class TestRomanNumeral(unittest.TestCase):
 
-    def test_ones(self):
+    def test_equal(self):
         self.assertEqual(rn.convert(1), 'I')
         self.assertEqual(rn.convert(2), 'II')
+        self.assertEqual(rn.convert(5), 'V')
+        self.assertEqual(rn.convert(9), 'IX')
+        self.assertEqual(rn.convert(10), 'X')
+        self.assertEqual(rn.convert(15), 'XV')
+        self.assertEqual(rn.convert(30), 'XXX')
+        self.assertEqual(rn.convert(40), 'XL')
+        self.assertEqual(rn.convert(50), 'L')
+        self.assertEqual(rn.convert(70), 'LXX')
+        self.assertEqual(rn.convert(90), 'XC')
+        self.assertEqual(rn.convert(93), 'XCIII')
+        self.assertEqual(rn.convert(100), 'C')
+        self.assertEqual(rn.convert(304), 'CCCIV')
+        self.assertEqual(rn.convert(519), 'DXIX')
+        self.assertEqual(rn.convert(699), 'DCXCIX')
+        self.assertEqual(rn.convert(961), 'CMLXI')
+        self.assertEqual(rn.convert(1000), 'M')
+        self.assertEqual(rn.convert(2804), 'MMDCCCIV')
+        self.assertEqual(rn.convert(3999), 'MMMCMXCIX')
 
 
 if __name__ == '__main__':
